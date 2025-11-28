83d632eb420ec1caef34d85e41e1e3ee99f1266f
Paul Pathode <paulpathode510@gmail.com>
2025-11-28 18:21:52 +0530

    Initial commit: project structure with frontend and backend

---
+++ HarmonyHub-backend/src/main/java/com/soft/HarmonyHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonyHubBackendApplication.class, args);
+	}
+
+}
---
+++ HarmonyHub-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  
+
+  return (
+      <h1 class="text-3xl font-bold underline bg-red-500">
+    Hello world!
+  </h1>
+  )
+}
+
+export default App
